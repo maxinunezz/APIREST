4be279b03fd3abec09b272cd64fb622671be1f45
maxinunezz <maxicbapl@gmail.com>
2024-03-12 15:43:21 -0300

    rouute post

--- routes/movies.js
+++ routes/movies.js
@@ -6,5 +6,17 @@ const movies = require("../sample.json");
 router.get("/", (req, res) => {
   res.json(movies);
 });
+router.post("/", (req, res) => {
+  const atributos = req.body;
+  console.log("atributossss", atributos);
+  if (atributos.nombre && atributos.director && atributos.clasificacion) {
+    const id = movies.length + 1;
+    const newMovie = { ...req.body, id };
+    movies.push(newMovie);
+    res.json(movies);
+  } else {
+    console.log("error");
+  }
+});
 
 module.exports = router;
